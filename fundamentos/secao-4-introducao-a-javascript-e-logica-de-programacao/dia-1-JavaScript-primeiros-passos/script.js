63e3b1b4e4424c955f4a7fc0a332c219c534e601
Vomelhor <vitorpnn2009@hotmail.com>
2022-09-21 15:50:39 -0500

    Adiciona variaveis e tipos primitivos ao script.js

---
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/script.js
@@ -0,0 +1,37 @@
+//Variaveis
+
+/*
+let myName = 'Vitor';
+const birthCity = 'Pontalina';
+let birthYear = 1996;
+console.log(myName + birthCity + birthYear);
+birthYear = 2030;
+console.log(birthYear);
+birthCity = 'Wisconsin Dells';
+console.log(birthCity);
+
+Mensagem de erro pois nao e possivel mudar uma constante.
+*/
+
+
+
+
+//Tipos Primitivos, Tipagem Dinâmica e Operações Aritméticas
+
+/*
+const base = 5;
+const heigth = 8;
+const area = base * heigth;
+console.log(area);
+const perimeter = base * 2 + heigth * 2;
+console.log(perimeter);
+*/
+
+
+
+
+
+//Condições If e Else
+
+///*
+const nota;
